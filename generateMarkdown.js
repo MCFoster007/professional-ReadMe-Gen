8443eb34b2b2bb0862845282412c34ae3521f001
mcfoster007 <mariechantalfoster@gmail.com>
2024-10-12 00:56:41 -0400

    added code to generator

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -2,44 +2,42 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === "Apache") {
-    return [![License](https://choosealicense.com/licenses/apache-2.0/)];
+    return `![License](https://choosealicense.com/licenses/apache-2.0/)`;
   } else if (license === "MIT") {
-    return "search again";
-
-  }
-  };
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license === "MIT") {
-    return [![License](https://choosealicense.com/licenses/mit/)];
+    return `![License](https://choosealicense.com/licenses/mit/)`;
   } else if (license === "Mozilla") {
-    return "search again";
-
+    return `![License](https://choosealicense.com/licenses/mpl-2.0/)`;
   }
-  };
-  
-
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license === "Mozilla") {
-    return [![License](https://choosealicense.com/licenses/mpl-2.0/)];
-  } else if (license === "Apache") {
-    return "search again";
-
-  }
-  };
-
-
-
-// TODO: Create a function to generate markdown for README. // like might be from the mini proj except with readme template and not html template
-function generateMarkdown(data) {
-  return `# ${data.title} 
-
-`;
+  return ''; 
 }
 
+// Create a function that returns the license link
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return `[MIT License](https://choosealicense.com/licenses/mit/)`;
+  } else if (license === "Apache") {
+    return `[Apache License](https://choosealicense.com/licenses/apache-2.0/)`;
+  } else if (license === "Mozilla") {
+    return `[Mozilla License](https://choosealicense.com/licenses/mpl-2.0/)`;
+  }
+  return ''; 
+}
+
+// Create a function that returns the license section of README
+function renderLicenseSection(license) {
+  if (license) {
+    return `## License\nThis project is licensed under the ${license}.\n${renderLicenseLink(license)}`;
+  }
+  return ''; 
+
+// Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title} 
+${renderLicenseBadge(data.license)}
+
+${renderLicenseSection(data.license)}
+`;
+
+}
 export default generateMarkdown;
+
